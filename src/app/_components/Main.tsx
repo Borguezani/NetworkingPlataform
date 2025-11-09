5f7aca17d075bad9881f585ea37307bd04ce81ab
Daniel Borguezani <daniel_borguezani@hotmail.com>
2025-11-09 17:48:09 -0300

    feat: Adicionar componente Main para estruturação de layout

---
+++ src/app/_components/Main.tsx
@@ -0,0 +1,6 @@
+
+export default function Main({ children }: { children: React.ReactNode }) {
+  return (
+    <main className="max-w-[1216px] mx-auto py-5 flex flex-col gap-1 items-center">{children}</main>
+  )
+}
